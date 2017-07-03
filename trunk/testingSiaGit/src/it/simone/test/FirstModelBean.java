c78fba0ec7f9a08d8a45021b90e7d3b432f36d7f
SimonePDev <spessis@gmail.com>
2017-07-03 15:26:47 +0200

    aggiunta jsf, primefaces

---
+++ trunk/testingSiaGit/src/it/simone/test/FirstModelBean.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package it.simone.test;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ * 
+ * @author simone.pessi -- 03 lug 2017
+ */
+@ManagedBean
+@ViewScoped
+public class FirstModelBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
